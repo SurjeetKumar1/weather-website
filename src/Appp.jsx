dd47bb31ca9779d6900ee7439df263e482a35e80
surjeet <ak2691622@gmail.com>
2024-01-20 23:18:48 +0530

    Your commit message here

---
+++ src/Appp.jsx
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from "react";
+ import "./css_component/card.css";
+
+const Appp=()=>{
+    const[name,setName]=useState("null ")
+    const[set,setinput]=useState("mumbai");
+
+
+    useEffect(()=>{
+    const url=`https://api.openweathermap.org/data/2.5/weather?q=${set}&units=metric&appid=556f7527eb35724ea29b373bc60e2ead`
+
+    async function weatherData(){
+   const res=await fetch(url);
+   const data= await res.json();
+   console.log(data.main)
+   setName(data.main)
+    }
+    weatherData();
+  },[set])
+
+  return(
+    <>
+    <div className="container">
+      <div className="maincard">
+        <div className="textcard">
+          <input type="search"  placeholder="eg. india" className="inpute" 
+            onChange={(element)=>{
+                setinput(element.target.value)  
+            }}
+          />
+        </div>
+        <div className="cityname">
+          <h1>{set}</h1>
+          <h2 className="temp">
+          {name? name.temp:""}
+          </h2>
+          <h3>{name?name.temp_min:""} °C || {name? name.temp_max:""}  °C</h3>
+        </div>
+      </div>
+      </div>
+    </>
+  )
+}
+export default Appp;
